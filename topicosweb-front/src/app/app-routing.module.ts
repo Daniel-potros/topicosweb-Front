3368a870573a9fd8f252af058af727c199aa2755
Daniel-potros <56022189+Daniel-potros@users.noreply.github.com>
2021-11-28 19:41:40 -0700

    routes

--- topicosweb-front/src/app/app-routing.module.ts
+++ topicosweb-front/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrderComponent } from './pages/order/order.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  }, 
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
